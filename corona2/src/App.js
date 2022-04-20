4248a834ed83b61ee96b9d55cdfa7ba7c7a11ffe
Shamith Pasula <shamith09@berkeley.edu>
2022-04-20 15:15:08 -0700

    commit

--- corona2/src/App.js
+++ corona2/src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import Test from './components/Test.js';
-import { Map, TileLayer} from 'react-leaflet';
+import L from 'leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 function App() {
+  delete L.Icon.Default.prototype._getIconUrl;
+
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  });
+
+  const location = [38.9072, -77.0369];
+  const zoom = 12;
+
   return (
     <div className="App">
-      <Map center = {[0,0]} zoom = {4}>
-        <TileLayer url = "https://x{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" ></TileLayer>
-      </Map>
+      <MapContainer center = {location} zoom = {zoom}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'></TileLayer>
+        <Marker position={location}></Marker>
+      </MapContainer>
     </div>
   );
 }
